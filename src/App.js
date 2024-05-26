7ae9f62b4e0b10d53cc4a86f155364b53b858a3d
evsolr <142960987+evsolr@users.noreply.github.com>
2024-05-26 13:09:34 +0530

    first commit
    new file:   .gitignore
    new file:   README.md
    new file:   package-lock.json
    new file:   package.json
    new file:   public/index.html
    new file:   src/App.css
    new file:   src/App.js
    new file:   src/index.css
    new file:   src/index.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import WeatherApp from './Components/WeatherApp';
+
+
+function App() {   
+  return (
+    <div>
+      <div>
+          <WeatherApp />
+      </div>
+      <hr/>
+    </div>
+    
+  );
+}
+
+export default App;
